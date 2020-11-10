d4e08f970418a4a2c31126d8613d17c56410eb63
Mikael FLORA <5983947+mikaelflora@users.noreply.github.com>
2020-11-10 18:19:15 +0100

    modification du champ password pour le formulaire credentials

--- src/Form/CredentialsType.php
+++ src/Form/CredentialsType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Credentials;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,9 @@ class CredentialsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('url')
-            ->add('login')
-            ->add('password')
+            ->add('url', TextType::class)
+            ->add('login', TextType::class)
+            ->add('password', PasswordType::class)
         ;
     }
 
