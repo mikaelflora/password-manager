435ed04407cbf0ef3823a51b47d9f30953cbfd3e
Mikael FLORA <5983947+mikaelflora@users.noreply.github.com>
2020-11-12 15:43:27 +0100

    suppression de son propre compte

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use App\Form\ChangePasswordType;
+use App\Form\RemoveAccountType;
 use App\Repository\CredentialRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -77,6 +79,46 @@ class AccountController extends AbstractController
         }
 
         return $this->render('account/password.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/account/remove", name="account_remove")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function remove(Request $request, UserPasswordEncoderInterface $encoder, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(RemoveAccountType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $form->get('password')->getData();
+
+            if ($encoder->isPasswordValid($user, $password)) {
+                $session = $this->get('session');
+                $session = new Session();
+                $session->invalidate();
+
+                $entityManager->remove($this->getUser());
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_register');
+            } else {
+                $this->addFlash(
+                    'danger',
+                    "Invalid password!"
+                );
+            }
+        }
+
+        return $this->render('account/remove.html.twig', [
             'form' => $form->createView(),
         ]);
     }
